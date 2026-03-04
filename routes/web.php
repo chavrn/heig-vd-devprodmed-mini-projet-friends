80b95b07f8444c42c067bf0b2548d34519c8405c
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-03-04 14:24:07 +0100

    Créer le cours 4 (#24)

--- routes/web.php
+++ routes/web.php
@@ -1,76 +1,20 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Models\Post;
-use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test-user', function () {
-    $user = new User();
+Route::get('/', function () {
+    $posts = Post::orderBy('created_at', 'desc')->with('user')->with('likes')->limit(3)->get();
 
-    $user->first_name = 'John';
-    $user->last_name = 'Doe';
-    $user->username = 'johndoe';
-    $user->email = 'johndoe@example.com';
-
-    $user->save();
-
-    return $user;
-});
-
-Route::get('/test-post-1', function () {
-    // Récupère la première personne dans la table `users`, peu importe son ID
-    $user = User::first();
-
-    $post = new Post();
-    $post->title = 'Mon premier post';
-    $post->content = 'Ceci est le contenu de mon premier post.';
-
-    $user->posts()->save($post);
-
-    return $post;
-});
-
-Route::get('/test-post-2', function () {
-    // Récupère la personne avec l'ID 1 dans la table `users`
-    $user = User::find(1);
-
-    $post = new Post();
-    $post->content = 'Ceci est le contenu de mon deuxième post.';
-
-    $post->user()->associate($user);
-
-    $post->save();
-
-    return $post;
-});
-
-Route::get('/test-like', function () {
-    $user = User::find(1);
-    $post = Post::find(2);
-
-    $user->likes()->attach($post->id, ['reaction' => 'love']);
-
-    return $post->likes;
+    return view('home', ['posts' => $posts]);
 });
 
 Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/', function () {
-    $posts = Post::orderBy('created_at', 'desc')->with('user')->with('likes')->get();
+Route::get('/@{username}', [ProfileController::class, 'show'])->where('username', '[A-Za-z0-9-_]+');
 
-    return view('home', ['posts' => $posts]);
-});
-
-Route::get('/profile', function () {
-    $user = User::where('username', 'janedoe')->first();
-
-    $posts = Post::where('user_id', $user->id)
-        ->orderBy('created_at', 'desc')
-        ->with(['user', 'likes'])
-        ->get();
-
-    return view('profile', ['user' => $user, 'posts' => $posts]);
-});
+Route::resource('posts', PostController::class);
